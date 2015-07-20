4b65423ddac84fd78c17c17767e966468307cf58
miya-masa <miyauchi0623@gmail.com>
2015-07-20 16:11:36 +0900

    コメントの追加

--- app/scripts/person.js
+++ app/scripts/person.js
@@ -1,6 +1,10 @@
 (function(window) {
   'use strict';
   var app = window.app || {};
+  // app.Person
+  // nameとgreetingを持つクラス
+  // helloMessageを関数として持つ
+  // 詳しい使用はpersonSpec.jsを参照すること
   app.Person = function(name, greeting) {
     this.name = name;
     this.greeting = greeting;
--- app/scripts/superman.js
+++ app/scripts/superman.js
@@ -1,6 +1,14 @@
 (function(window) {
   'use strict';
   var app = window.app || {};
+  // app.Superman
+  // app.Personを親とする
+  // コンストラクタで親のコンストラクタをコール
+  // プロトタイプはObject.createを利用する
+  // 子供特有のメソッドは以下の通り
+  // fly()
+  // superHelloMessage()
+  // 仕様についてはsupermanSpec.jsを参照すること
   app.Superman = function(name, greeting) {
     // 親のコンストラクタを呼ぶ簡単な方法
     app.Person.call(this, name, greeting);
