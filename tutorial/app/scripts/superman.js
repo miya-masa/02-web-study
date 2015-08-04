c287d67eece5754538c97c27c736e7be4184ea4b
miya-masa <miyauchi0623@gmail.com>
2015-08-05 07:30:12 +0900

    tutorial を修正

--- tutorial/app/scripts/superman.js
+++ tutorial/app/scripts/superman.js
@@ -1,6 +1,7 @@
 (function(window) {
   'use strict';
   var app = window.app || {};
+  /* Question */
   // app.Superman
   // app.Personを親とする
   // コンストラクタで親のコンストラクタをコール
@@ -9,25 +10,5 @@
   // fly()
   // superHelloMessage()
   // 仕様についてはsupermanSpec.jsを参照すること
-  app.Superman = function(name, greeting) {
-    // 親のコンストラクタを呼ぶ簡単な方法
-    app.Person.call(this, name, greeting);
-  };
-  // 指定したプロトタイプを持つオブジェクトの生成
-  // つまりSuperman.prototypeはPerson.prototypeを参照するオブジェクト
-  app.Superman.prototype = Object.create(app.Person.prototype, {
-    constructor: {
-      value: app.Superman,
-      enumerable: false,
-      writable: true,
-      configurable: true
-    }
-  });
-  app.Superman.prototype.superHelloMessage = function() {
-    return this.greeting + ' Superman ' + this.name;
-  };
-  app.Superman.prototype.fly = function() {
-    console.log('I can fly. Booooooon.');
-  };
   window.app = app;
 })(window);
